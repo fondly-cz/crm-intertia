d0715412ea16f0ebb05f0e6218acf08501878d83
MartinKokesCz <info@martinkokes.cz>
2026-02-27 12:20:01 +0100

    :link: Change copy link to direct public URL link in calculation index

--- resources/js/Pages/Calculations/Index.vue
+++ resources/js/Pages/Calculations/Index.vue
@@ -118,15 +118,16 @@
                             </td>
                             <td class="px-8 py-6 text-right">
                                 <div class="flex justify-end gap-2">
-                                    <button 
-                                        @click="copyLink(calc.public_url)" 
+                                    <a 
+                                        :href="calc.public_url" 
+                                        target="_blank"
                                         class="p-2.5 bg-gray-50 text-gray-400 rounded-xl hover:text-brand-primary-from hover:bg-white hover:shadow-sm transition-all"
-                                        title="Kopírovat veřejný odkaz"
+                                        title="Otevřít veřejný odkaz"
                                     >
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                                         </svg>
-                                    </button>
+                                    </a>
                                     <Link 
                                         :href="`/calculations/${calc.id}/edit`"
                                         class="p-2.5 bg-gray-50 text-gray-400 rounded-xl hover:text-brand-primary-from hover:bg-white hover:shadow-sm transition-all"
